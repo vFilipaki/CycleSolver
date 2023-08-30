53445bf325e86853efc69321febdcdcc3374b137
vFilipaki <131402657+vFilipaki@users.noreply.github.com>
2023-08-30 10:42:06 -0300

    Update runtests.jl - commented code to test the documentation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, Documenter, CycleSolver
 
-include("equations.test.jl")
+# include("equations.test.jl")
 
-@testset "DocTests for CycleSolver" begin
-    doctest(CycleSolver; manual = false)
-end
+# @testset "DocTests for CycleSolver" begin
+#     doctest(CycleSolver; manual = false)
+# end
