dec52d58339ccde423db6242360d0b4fb698bce2
vFilipaki <vifilipaki@gmail.com>
2023-09-11 22:47:25 -0300

    Update runtest - others tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,8 @@ using Test, CycleSolver
     @testset "States" begin include("states.test.jl"); end
     @testset "Mass flow" begin include("massFlowManager.test.jl"); end
     @testset "Components" begin include("components.test.jl"); end
+    @testset "Cycle Properties" begin include("cycleProperties.test.jl"); end
+    @testset "System Properties" begin include("systemProperties.test.jl"); end
+    @testset "Thermo Properties" begin include("thermoProperties.test.jl"); end
+    @testset "Hypotheses" begin include("hypotheses.test.jl"); end
 end
