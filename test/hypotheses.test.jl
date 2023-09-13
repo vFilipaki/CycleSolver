c1dc19879d23b51d526d6643cafc08c136b8e5f6
vFilipaki <vifilipaki@gmail.com>
2023-09-13 16:49:09 -0300

    update hypotheses.test - more tests added

--- test/hypotheses.test.jl
+++ test/hypotheses.test.jl
@@ -19,7 +19,7 @@ using Test, CycleSolver
     "bVars ~ 150"
 end
 
-@testset "hypotheses.test.jl: Solver with hypotheses" begin
+@testset "hypotheses.test.jl: Solver with hypotheses [Find entropy]" begin
     CycleSolver.ClearSystem()
     
     CycleSolver.NewEquation(:(st2.h = st1.h - (st1.h - st2s.h) * 0.8))
@@ -41,4 +41,49 @@ end
     CycleSolver.SolverWithHypotheses("", solutionFinded)
 
     @test string(solutionFinded[1]) == "Any[:(st2.s), 7.88496093749998]"
+end
+
+@testset "hypotheses.test.jl: Solver with hypotheses [Find pressure]" begin
+    CycleSolver.ClearSystem()
+    
+    CycleSolver.NewEquation(:(st2.h = st1.h - (st1.h - st2s.h) * 0.8))
+    CycleSolver.NewEquation(:(st1.T = 500))
+    CycleSolver.NewEquation(:(st1.p = 200))
+    CycleSolver.NewEquation(:(st2.Q = 0.95))
+    CycleSolver.NewEquation(:(st1.s = st2s.s))
+    CycleSolver.NewEquation(:(st2s.p = st2.p))
+
+    eval(:(CycleSolver.st1.fluid = "water"))
+    eval(:(CycleSolver.st2.fluid = "water"))
+    eval(:(CycleSolver.st2s.fluid = "water"))
+
+    CycleSolver.EquationsSolver(CycleSolver.unsolvedEquations)
+    CycleSolver.StatesSolver(CycleSolver.unsolvedStates)
+    CycleSolver.UpdateEquationList(CycleSolver.unsolvedEquations)
+
+    solutionFinded = []
+    CycleSolver.SolverWithHypotheses("", solutionFinded)
+
+    @test string(solutionFinded[1]) == "Any[:(st2.p), 4.69384765625]"
+end
+
+@testset "hypotheses.test.jl: Solver with hypotheses [Conditional Equation]" begin
+    CycleSolver.ClearSystem()
+    
+    push!(CycleSolver.unsolvedConditionalEquation, 
+    CycleSolver.ConditionalMathEq(
+        :(a == 0), 
+        [:(b = 1)],
+        [:(b = 0)]))
+
+    CycleSolver.NewEquation(:(a = b - 1))
+
+    CycleSolver.ManageConditionalEquations(CycleSolver.unsolvedConditionalEquation)
+    CycleSolver.EquationsSolver(CycleSolver.unsolvedEquations)
+    CycleSolver.UpdateEquationList(CycleSolver.unsolvedEquations)
+
+    solutionFinded = []
+    CycleSolver.SolverWithHypotheses("", solutionFinded)
+
+    @test string(solutionFinded[1]) == "Any[:((unsolvedConditionalEquation[1]).condition), true]"
 end
